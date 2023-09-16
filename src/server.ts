4ccdbf46f5b4c9b2c6745050c2ec560a7801fe20
GomesZz <ggvgabriel05@gmail.com>
2023-09-15 22:33:59 -0300

    principio do backend concluido

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
 import { fastify } from "fastify";
-import { prisma } from "./lib/prisma";
+import { fastifyCors } from "@fastify/cors";
+
 import { getAllPromptsRoute } from "./routers/get-all-prompts";
 import { uploadVideoRoute } from "./routers/upload-video";
 import { createTranscriptionRoute } from "./routers/create-transcription";
+import { generateAICompletionRoute } from "./routers/generate-ai-completion";
 
 const app = fastify();
 
+app.register(fastifyCors, {
+  origin: "*",
+  // colocar a url do front
+});
+
 app.register(getAllPromptsRoute);
+app.register(generateAICompletionRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
 
